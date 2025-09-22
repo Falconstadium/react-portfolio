1e178334d61f6c72cd275a2de376eccb8acd3020
Yassir <atertay@gmail.com>
2025-09-22 10:10:02 +0100

    refactor: add font to index.html

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ import { Projects } from "../components/Projects";
 
 const Home = () => {
   return (
-    <main className="font-main grid min-h-dvh grid-rows-[auto_1fr] bg-neutral-950 text-gray-50">
+    <main className="grid min-h-dvh grid-rows-[auto_1fr] bg-neutral-950 text-gray-50">
       <Navbar />
       <div>
         <div className="mx-auto grid min-h-dvh gap-12 py-10">
