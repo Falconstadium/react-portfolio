8afe22c863528b94c35b64bdb6697345dd1ab7aa
Yassir <atertay@gmail.com>
2025-09-27 17:58:10 +0100

    style: preloader

--- src/components/Preloading.tsx
+++ src/components/Preloading.tsx
@@ -1,6 +1,6 @@
 const Preloading = () => {
   return (
-    <div className="size-32 animate-spin rounded-full border-2 border-gray-600 border-t-blue-600" />
+    <div className="size-20 animate-spin rounded-full border-4 border-gray-600 border-t-blue-600 md:size-32 md:border-2" />
   );
 };
 
