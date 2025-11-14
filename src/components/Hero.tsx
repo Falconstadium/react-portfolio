6e319d7162d574d45bc327507300ad2e8b1f8b84
Yassir <atertay@gmail.com>
2025-11-14 21:03:10 +0100

    style: edit animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,7 +14,7 @@ export const Hero = () => {
         initial={{ y: -50, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.3, ease: "easeOut", type: "tween" }}
-        className="img shadow-dark mx-auto mb-4 size-32 bg-[url('/photo-profile.png')] bg-cover bg-center bg-no-repeat md:mx-0"
+        className="img shadow-dark mx-auto mb-4 size-36 bg-[url('/photo-profile.png')] bg-cover bg-center bg-no-repeat md:mx-0"
       />
       <motion.h1
         initial={{ y: 50, opacity: 0 }}
@@ -29,7 +29,7 @@ export const Hero = () => {
         initial={{ y: 50, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 1, delay: 0.2, type: "spring" }}
-        className="text-sm tracking-wider text-neutral-400"
+        className="text-sm text-neutral-400"
       >
         {resources[lang].hero.title}
         <span> {resources[lang].hero.desc}</span>
