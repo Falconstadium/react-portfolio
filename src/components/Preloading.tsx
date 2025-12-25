6cb0021c84d45be0e469fe926e3b5d2bf7c79fea
Yassir <atertay@gmail.com>
2025-12-25 12:20:40 +0100

    style: edit the border

--- src/components/Preloading.tsx
+++ src/components/Preloading.tsx
@@ -1,6 +1,6 @@
 const Preloading = () => {
   return (
-    <div className="size-14 animate-spin rounded-full border-4 border-neutral-400 border-t-blue-600 md:size-20 md:border-8" />
+    <div className="size-14 animate-spin rounded-full border-4 border-neutral-400 border-t-blue-600 md:size-20 md:border-[6px]" />
   );
 };
 
