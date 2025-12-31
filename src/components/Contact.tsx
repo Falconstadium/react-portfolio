98cabaa98b8c5335cce34d436fbb9e1eaa1cf06b
Yassir <atertay@gmail.com>
2025-12-31 16:03:05 +0100

    style: Add new link and change some css

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,45 +2,38 @@ import { motion } from "framer-motion";
 
 export const Contact = () => {
   return (
-    <section className="font-satoshi px-6 py-8" id="contact" dir="ltr">
-      <div className="mx-auto flex max-w-xl items-center justify-between">
-        <div className="grid gap-4">
-          <motion.div
-            initial={{ opacity: 0, x: -50 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            viewport={{ once: true, amount: "all" }}
-            transition={{ duration: 1 }}
-            className="flex items-center gap-3 text-neutral-400 transition-colors duration-300 hover:text-white active:text-white"
+    <section className="font-satoshi py-8" id="contact" dir="ltr">
+      <div className="mx-auto grid max-w-2xl place-items-center gap-6 md:grid-cols-2">
+        <div className="flex items-center gap-2 text-neutral-400 transition-colors duration-300 hover:text-white active:text-white">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="15"
+            height="15"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="lucide lucide-mail-icon lucide-mail"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="15"
-              height="15"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="lucide lucide-mail-icon lucide-mail"
-            >
-              <path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" />
-              <rect x="2" y="4" width="20" height="16" rx="2" />
-            </svg>
-            <a
-              href="mailto:aterta03@gmail.com"
-              className="text-xs font-medium tracking-wide md:text-sm"
-            >
-              aterta03@gmail.com
-            </a>
-          </motion.div>
+            <path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" />
+            <rect x="2" y="4" width="20" height="16" rx="2" />
+          </svg>
+          <a
+            href="mailto:aterta03@gmail.com"
+            className="text-xs font-medium tracking-wide md:text-sm"
+          >
+            aterta03@gmail.com
+          </a>
         </div>
-        <ul className="flex items-center justify-end gap-8 text-sm font-medium text-neutral-400">
+
+        <ul className="flex items-center justify-end gap-6 text-xs font-medium text-neutral-400 md:text-sm">
           <motion.li
             initial={{ y: -50, opacity: 0 }}
             whileInView={{ y: 0, opacity: 1 }}
             viewport={{ once: true, amount: "all" }}
-            transition={{ duration: 1 }}
+            transition={{ duration: 1, ease: "easeOut" }}
           >
             <a
               href="https://www.linkedin.com/in/yassiraterta"
@@ -55,7 +48,7 @@ export const Contact = () => {
             initial={{ y: -50, opacity: 0 }}
             whileInView={{ y: 0, opacity: 1 }}
             viewport={{ once: true, amount: "all" }}
-            transition={{ duration: 1, delay: 0.2 }}
+            transition={{ duration: 1, delay: 0.2, ease: "easeOut" }}
           >
             <a
               href="https://github.com/Falconstadium"
@@ -70,7 +63,7 @@ export const Contact = () => {
             initial={{ y: -50, opacity: 0 }}
             whileInView={{ y: 0, opacity: 1 }}
             viewport={{ once: true, amount: "all" }}
-            transition={{ duration: 1, delay: 0.2 }}
+            transition={{ duration: 1, delay: 0.3, ease: "easeOut" }}
           >
             <a
               href="https://www.instagram.com/yassirdevs/"
